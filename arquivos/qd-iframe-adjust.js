46059dfe6542e4cf983367525fae8e2e38625902
Carlos Vinicius <carlosjp@hotmail.co.jp>
2015-08-11 17:06:41 -0300

    -

--- arquivos/qd-iframe-adjust.js
+++ arquivos/qd-iframe-adjust.js
@@ -1,3 +1,9 @@
 (function(){
-  
+	// setIframeSize($("body").outerHeight(true) + 5);
+	
+	console.log(window);
+
+	jQuery(window).scroll(function(){
+		// setIframeSize($("body").outerHeight(true) + 5);
+	});
 })();
