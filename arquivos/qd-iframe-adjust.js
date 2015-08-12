e479c8eb4315f1308a90a1f724a06fd2a042db05
Carlos Vinicius <carlosjp@hotmail.co.jp>
2015-08-12 16:04:23 -0300

    ---

--- arquivos/qd-iframe-adjust.js
+++ arquivos/qd-iframe-adjust.js
@@ -1,4 +1,9 @@
 (function(){
+	if(window.parent === window)
+		location.href = location.href.replace("qd-araujo-cdn.github.io", "www.araujo.com.br");
+})();
+
+(function(){
 	$(function() {
 		resetIframeSize();
 		window.parent.postMessage("qd-iframe-cdn|" + ($(document).height() + 5), "*");
