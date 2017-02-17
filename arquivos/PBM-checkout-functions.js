2c290b3e393757d641a59073f50c45fc1fb4a68e
Perillo Santana <perillo.santana@quatrodigital.com.br>
2017-02-17 16:32:01 -0200

    ....

--- arquivos/PBM-checkout-functions.js
+++ arquivos/PBM-checkout-functions.js
@@ -5,47 +5,20 @@ var QdPbmCheckout = {
 		QdPbmCheckout.checkRequestIsRunning();
 	},
 	init: function() {
+		QdPbmCheckout.loadElements(); // chamar antes de todos
+		QdPbmCheckout.fullPageNotification(); // chamar antes de todos
 		QdPbmCheckout.cookieRenew(); // chamar antes de todos
-		QdPbmCheckout.fullpageLoading(); // chamar antes de todos
+
 		QdPbmCheckout.validateItems();
 	},
-	fullpageLoading: function() {
-		$('.container-main').append('<div class="qd-fullpage-notification"></div>');
-	},
 	orderUpdated: function(orderForm){
-		if (typeof orderForm.messages == "object") {
-			for (i in orderForm.messages) {
-				if (orderForm.messages[i].text.indexOf($.cookie('qdPbm')) >= 0) {
-					var fullPage = $('.qd-fullpage-notification').html('<div class="qd-fullpage-notification-error"> <div class="qd-fullpage-notification-error-header"> <span>Não foi possível aplicar seu desconto do PBM</span> </div> <p><strong>Infelizmente ocorreu um erro ao tentar aplicar o seu desconto do PBM.</strong></p> <p>para tentar resolver esse erro, por favor verifique se os itens abaixo estão corretos: </p> <ul> <li>O <strong>CPF</strong> utilizado na página de produto <strong>deve ser o mesmo</strong> que esta utilizando para concluir a compra;</li> <li>A quantidade dos itens no carrinho é a mesma que foi utilziada para consultar o benefício na tela de produto.</li> </ul> <p>Caso esse error persista, por favor entre em contato com o Atendimento ao Cliente.</p> <p><strong>Atente-se que devido a este erro, para que o desconto do PBM lhe seja ocnedido novamente, será necessário que você volte a tela de produto e insira novamente o seu CPF.</strong></p> <a href="/checkout/#/cart" class="qd-fullpage-notification-close">Fechar</a> </div>');
+		if (location.hash.toLowerCase().indexOf('/payment') > -1)
+			QdPbmCheckout.checkRequestDetails(orderForm);
 
-					$('.vtex-front-messages-template').hide();
-
-					fullPage.find('.qd-fullpage-notification-close').click(function(evt) {
-						evt.preventDefault();
-						fullPage.toggle();
-					});
-
-					fullPage.toggle();
-
-					$.removeCookie('qdPbm', { path: '/' });
-
-					return;
-				} 
-				else if (orderForm.messages[i].text.indexOf('Vale Compra' >= 0)) {
-					var fullPage = $('.qd-fullpage-notification').html('<div class="qd-fullpage-notification-reload"> <div class="qd-fullpage-notification-reload-header"> <span>Ocorreu um erro ao aplicar o desconto PBM</span> </div> <p>Solicitamos que recarregue a pagina para tentar novamente.</p> <a href="/checkout/#/cart" class="qd-fullpage-notification-reload-link">Recarregar página</a> </div>');
-
-					$('.vtex-front-messages-template').hide();
-
-					fullPage.find('.qd-fullpage-notification-reload-link').click(function(evt) {
-						evt.preventDefault();
-						location.reload();
-					});
-
-					fullPage.toggle();
-					return;
-				}
-			}
-		}
+		QdPbmCheckout.validateItems();
+	},
+	cart: function() {
+		QdPbmCheckout.removeGiftcard();
 	},
 	payment: function() {
 		if(!QdPbmCheckout.requestRunning)
@@ -54,6 +27,74 @@ var QdPbmCheckout = {
 			$(window).one("checkoutRequestEnd.vtex", function() {
 				QdPbmCheckout.execPayment();
 			});
+	},
+	loadElements: function () {
+		QdPbmCheckout.cartElement = $('#cartLoadedDiv');
+	},
+	removeGiftcard: function() {
+		try {
+			// obtendo o indece do vale
+			var redemptionCode = $.cookie('qdPbm');
+			var index = -1;
+			if(vtexjs.checkout.orderForm.paymentData && vtexjs.checkout.orderForm.paymentData.giftCards){
+				for(var i = 0; i < vtexjs.checkout.orderForm.paymentData.giftCards.length; i++){
+					if(vtexjs.checkout.orderForm.paymentData.giftCards[i].redemptionCode == redemptionCode){
+						index = i;
+						break;
+					}
+				}
+			}
+
+			if(index < 0)
+				return;
+
+			vtexjs.checkout.orderForm.paymentData.giftCards.splice(index, 1);
+
+			vtexjs.checkout.sendAttachment('paymentData', {
+				giftCards: vtexjs.checkout.orderForm.paymentData.giftCards,
+				payments: vtexjs.checkout.orderForm.paymentData.payments
+			}).fail(QdPbmCheckout.notificationError);
+		}
+		catch (e) {(typeof console !== "undefined" && typeof console.error === "function" && console.error("Problemas :( . Detalhes: ", e)); }
+	},
+	fullPageNotification: function() {
+		QdPbmCheckout.fullPageElement = $('<div class="qd-fullpage-notification"></div>');
+		QdPbmCheckout.fullPageElement.appendTo('.container-main');
+	},
+	checkRequestDetails: function(orderForm) {
+		if (typeof orderForm.messages == "object") {
+			for (i in orderForm.messages) {
+				if (orderForm.messages[i].text.indexOf($.cookie('qdPbm')) >= 0) {
+					QdPbmCheckout.fullPageElement.html('<div class="qd-fullpage-notification-error"> <div class="qd-fullpage-notification-error-header"> <span>Não foi possível aplicar seu desconto do PBM</span> </div> <p><strong>Infelizmente ocorreu um erro ao tentar aplicar o seu desconto do PBM.</strong></p> <p>para tentar resolver esse erro, por favor verifique se os itens abaixo estão corretos: </p> <ul><li>A quantidade dos itens no carrinho é a mesma que foi utilziada para consultar o benefício na tela de produto.</li> </ul> <p>Caso esse error persista, por favor entre em contato com o Atendimento ao Cliente.</p> <p><strong>Atente-se que devido a este erro, para que o desconto do PBM lhe seja concedido novamente, será necessário que você volte a tela de produto e insira novamente o seu CPF.</strong></p> <a href="/checkout/#/cart" class="qd-fullpage-notification-close">Fechar</a> </div>');
+					QdPbmCheckout.fullPageElement.show();
+
+					$('.vtex-front-messages-template').hide();
+
+					QdPbmCheckout.fullPageElement.find('.qd-fullpage-notification-close').click(function(evt) {
+						evt.preventDefault();
+						QdPbmCheckout.fullPageElement.hide();
+					});
+
+					$.removeCookie('qdPbm', { path: '/' });
+					return;
+				}
+				else if (orderForm.messages[i].text.indexOf('Vale Compra' >= 0)) {
+					QdPbmCheckout.notificationError();
+					$('.vtex-front-messages-template').hide();
+					return;
+				}
+			}
+		}
+
+	},
+	notificationError: function() {
+		var fullPage = QdPbmCheckout.fullPageElement.html('<div class="qd-fullpage-notification-error"> <div class="qd-fullpage-notification-error-header"> <span>Ocorreu um erro ao aplicar o desconto PBM</span> </div> <p><center><strong>Solicitamos que recarregue a pagina para tentar novamente.</strong></center></p> <a href="/checkout/#/cart" class="qd-fullpage-notification-reload-link">Recarregar página</a> </div>');
+		QdPbmCheckout.fullPageElement.show();
+
+		QdPbmCheckout.fullPageElement.find('.qd-fullpage-notification-reload-link').click(function(evt) {
+			evt.preventDefault();
+			location.reload();
+		});
 	},
 	execPayment: function() {
 		// Verificando se o usuário esta logado
@@ -73,9 +114,6 @@ var QdPbmCheckout = {
 		});
 	},
 	attachmentOrder: function(data) {
-		if ($.cookie('qdPbm').length <= 0)
-			return;
-
 		$(document.body).addClass('qd-loading');
 
 		try {
@@ -100,15 +138,19 @@ var QdPbmCheckout = {
 					"discPerc": (data.items[i].PbmDiscount / 100 / 100).toFixed(4),
 					"qtyValid": data.items[i].checkoutValid
 				});
-			}
-			infoPbm["PBM"] = {
+			};
+			var newPbmData = {
 				"cpf": data.cpf,
 				'items': pbmItems
 			};
 
-			vtexjs.checkout.sendAttachment('openTextField', {value: JSON.stringify(infoPbm)}).always(function() {
-				$(document.body).removeClass('qd-loading');
-			});
+			if(!(infoPbm.PBM && JSON.stringify(infoPbm.PBM) == JSON.stringify(newPbmData))){
+				infoPbm['PBM'] = newPbmData;
+
+				vtexjs.checkout.sendAttachment('openTextField', {value: JSON.stringify(infoPbm)}).always(function() {
+					$(document.body).removeClass('qd-loading');
+				});
+			}
 		} catch (e) {(typeof console !== "undefined" && typeof console.error === "function" && console.error("Problemas :( . Detalhes: ", e)); }
 	},
 	cookieRenew: function() {
@@ -120,10 +162,21 @@ var QdPbmCheckout = {
 		else
 			callback();
 	},
-	itemsValidated: function() {
+	itemsValidated: function(data) {
 		try {
-			var fullPage = $('.qd-fullpage-notification').html('<div class="qd-fullpage-notification-applying"> <div class="qd-fullpage-notification-applying-header"> <img src="/arquivos/stamp-pbm-2.jpg" alt="PBM" /> </div> <p>Aguarde. Estamos aplicando o seu desconto.</p> <i class="icon-spinner icon-spin"></i> </div>');
-			fullPage.show();
+			if (location.hash.toLowerCase().indexOf('/payment') < 0 || !(data.giftcardValue > 0 && !data.checkItemsAgain))
+				return;
+
+			// Verifico se já não tenho esse gift aplicado
+			var redemptionCode = $.cookie('qdPbm');
+			if(vtexjs.checkout.orderForm.paymentData && vtexjs.checkout.orderForm.paymentData.giftCards)
+				for(var i = 0; i < vtexjs.checkout.orderForm.paymentData.giftCards.length; i++){
+					if(vtexjs.checkout.orderForm.paymentData.giftCards[i].redemptionCode == redemptionCode)
+						return $('.gift-card-section').addClass('qd-pbm-applied-discount');
+				}
+
+			QdPbmCheckout.fullPageElement.html('<div class="qd-fullpage-notification-applying"> <div class="qd-fullpage-notification-applying-header"> <img src="/arquivos/stamp-pbm-2.jpg" alt="PBM" /> </div> <p>Aguarde. Estamos aplicando o seu desconto.</p> <i class="icon-spinner icon-spin"></i> </div>');
+			QdPbmCheckout.fullPageElement.show();
 
 			$.ajax({
 				url: '/api/checkout/pub/gift-cards/providers',
@@ -133,36 +186,24 @@ var QdPbmCheckout = {
 			}).done(function(data) {
 				vtexjs.checkout.sendAttachment('paymentData', {
 					giftCards: [{
-						inUse : true,
-						isSpecialCard : false,
-						provider : data[0].id,
-						redemptionCode : $.cookie('qdPbm')
+						inUse: true,
+						isSpecialCard: false,
+						provider: data[0].id,
+						redemptionCode: redemptionCode
 					}],
 					payments: vtexjs.checkout.orderForm.paymentData.payments
+				}).done(function() {
+					QdPbmCheckout.fullPageElement.hide();
 				}).always(function() {
-					fullPage.toggle();
-					$('.payment-discounts-list').addClass('qd-pbm-applied-discount');
-				}).fail(function() {
-					fullPage.html('<div class="qd-fullpage-notification-reload"> <div class="qd-fullpage-notification-reload-header"> <span>Ocorreu um erro ao aplicar o desconto PBM</span> </div> <p>Solicitamos que recarregue a pagina para tentar novamente.</p> <a href="/checkout/#/cart" class="qd-fullpage-notification-reload-link">Recarregar página</a> </div>');
+					$('.gift-card-section').addClass('qd-pbm-applied-discount');
+				}).fail(QdPbmCheckout.notificationError);
+			}).fail(QdPbmCheckout.notificationError);
+		} catch (e) {(typeof console !== "undefined" && typeof console.error === "function" && console.error("Problemas :( . Detalhes: ", e)); }
 
-					fullPage.find('.qd-fullpage-notification-reload-link').click(function(evt) {
-						evt.preventDefault();
-						location.reload();
-					});
-
-					fullPage.toggle();
-				});
-			});
-		}
-		catch (e) {
-			$('.vtex-front-messages-modal-template').modal('hide');
-		}
 	},
 	validateItems: function() {
 		if (!vtexjs.checkout.orderForm || vtexjs.checkout.orderForm.items.length <= 0)
 			return;
-
-		var productItem = $('.cart-items');
 
 		$(document.body).addClass('qd-loading');
 		$.ajax({
@@ -175,62 +216,60 @@ var QdPbmCheckout = {
 			},
 			complete: function() { $(document.body).removeClass('qd-loading') }
 		}).done(function (data) {
-			if (data.giftcardValue <= 0) {
-				var fullPage = $('.qd-fullpage-notification').html('<div class="qd-fullpage-notification-not-applied"> <div class="qd-fullpage-notification-not-applied-header"> <span>Atenção!</span> </div> <p>O desconto da Drogaria Araujo é maior do que o oferecido pelo PBM.</p> <a href="/checkout/#/cart" class="qd-fullpage-notification-close">Fechar</a> </div>');
-
-				fullPage.find('.qd-fullpage-notification-close').click(function(evt) {
-					evt.preventDefault();
-					fullPage.toggle();
-				});
-
-				fullPage.toggle();
-				return;
-			}
-
-
-			if(!data.pbmAvailable || !data.discountAvailable)
-				return;
-
-			var checkReq = function() {
-				if(req != cReq)
+			try {
+				if(!data.pbmAvailable || !data.discountAvailable)
 					return;
-				if(req != 0)
-					QdPbmCheckout.validateItems();
-				if(req == cReq) {
-					QdPbmCheckout.itemsValidated();
-					QdPbmCheckout.attachmentOrder(data);
-				}
-			};
 
-			var req = 0;
-			var cReq = 0;
-			for(var i = 0; i < data.items.length; i++){
-				if(!data.items[i].Pbm)
-					continue;
+				if (data.giftcardValue <= 0 && !data.checkItemsAgain) {
+					QdPbmCheckout.fullPageElement.html('<div class="qd-fullpage-notification-not-applied"> <div class="qd-fullpage-notification-not-applied-header"> <span>Atenção!</span> </div> <p>O desconto da Drogaria Araujo é maior do que o oferecido pelo PBM.</p> <a href="/checkout/#/cart" class="qd-fullpage-notification-close">Fechar</a> </div>');
+					QdPbmCheckout.fullPageElement.show();
 
-				if(data.items[i].PbmValid){
-					checkReq();
-					continue;
-				}
-				else{
-					req++;
-					QdPbmCheckout.preAuth(data, data.items[i]).always(function() {
-						cReq++;
-						checkReq();
+					QdPbmCheckout.fullPageElement.find('.qd-fullpage-notification-close').click(function(evt) {
+						evt.preventDefault();
+						QdPbmCheckout.fullPageElement.hide();
 					});
-				}
-			}
 
-			window.dataJson = data;
-			setTimeout(function(){
-				for (item in data.items) {
-					if (data.items[item].Pbm) {
-						var priceDiscount = Math.ceil(data.items[item].listPrice * ((100- data.items[item].PbmDiscount /100)/100));
-						productItem.find('.product-item[data-sku="' + data.items[item].id + '"] td.product-price').append('<div class="qd-pbm-item">  <span>Valor com o desconto do PBM: <span class="qd-pbm-item-value">R$ ' + qd_number_format(priceDiscount / 100, 2, ",", ".") + '</span></span> </div>');
+					return;
+				}
+
+				var checkReq = function() {
+					if(req != cReq)
+						return;
+					if(req != 0)
+						QdPbmCheckout.validateItems();
+					if(req == cReq) {
+						QdPbmCheckout.itemsValidated(data);
+						QdPbmCheckout.attachmentOrder(data);
+					}
+				};
+
+				var req = 0;
+				var cReq = 0;
+				for(var i = 0; i < data.items.length; i++){
+					if(!data.items[i].Pbm)
+						continue;
+
+					if(data.items[i].PbmValid){
+						QdPbmCheckout.showCartDiscountInformation(data.items[i], i);
+						checkReq();
+						continue;
+					}
+					else{
+						req++;
+						QdPbmCheckout.preAuth(data, data.items[i]).always(function() {
+							cReq++;
+							checkReq();
+						});
 					}
 				}
-			}, 4000);
+			}
+			catch (e) {(typeof console !== "undefined" && typeof console.error === "function" && console.error("Problemas :( . Detalhes: ", e)); }
 		});
+	},
+	showCartDiscountInformation: function(item, index) {
+		var priceDiscount = Math.ceil(item.listPrice * ((100 - item.PbmDiscount /100)/100));
+		QdPbmCheckout.cartElement.find('.qd-pbm-item').remove();
+		QdPbmCheckout.cartElement.find('.product-item[data-sku="' + item.id + '"] td.product-price').append('<div class="qd-pbm-item">  <span>Valor com o desconto do PBM: <span class="qd-pbm-item-value">R$ ' + qd_number_format(priceDiscount / 100, 2, ",", ".") + '</span></span> </div>');
 	},
 	preAuth: function(data, item) {
 		return $.ajax({
@@ -248,10 +287,9 @@ var QdPbmCheckout = {
 		});
 	}
 };
+
 (QdPbmCheckout.run)();
-
 $(QdPbmCheckout.init);
-
 $(window).on("orderFormUpdated.vtex", function(e, data){
 	QdPbmCheckout.orderUpdated(data)
 });
