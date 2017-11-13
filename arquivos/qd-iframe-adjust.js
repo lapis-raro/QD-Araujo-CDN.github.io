67f08f8b10fbd08dd221b8cf9842a4263d37b6b7
Carlos Santos <carlos@quatrodigital.com.br>
2017-11-13 16:34:29 -0200

    ---

--- arquivos/qd-iframe-adjust.js
+++ arquivos/qd-iframe-adjust.js
@@ -1,37 +1,39 @@
-(function(){
-	if(window.parent === window)
+(function () {
+	if (window.parent === window)
 		location.href = location.href.replace("qd-araujo-cdn.github.io", "www.araujo.com.br");
 })();
 
-(function(){
+(function ($) {
 	"use strict";
 
 	function resetIframeSize(useBody) {
-		window.parent.postMessage("qd-iframe-cdn|" + ($(useBody? document.body: document).height() + 5), "*");
+		window.parent.postMessage("qd-iframe-cdn|" + ($(useBody ? document.body : document).height() + 5), "*");
 	};
 
-	$(function() {resetIframeSize(false); });
-	$(window).load(function() {resetIframeSize(false); });
-	$(window).scroll(function() {resetIframeSize(false); });
-	$(window).on('QD_manualIframeAdjust', function(e, useBody) {resetIframeSize(useBody || false); });
-	$(document).ajaxComplete(function() {resetIframeSize(false); });
-	$(document).ajaxStart(function() {resetIframeSize(false); });
+	$(function () { resetIframeSize(false); });
+	$(window).load(function () { resetIframeSize(false); });
+	$(window).scroll(function () { resetIframeSize(false); });
+	$(window).on('QD_manualIframeAdjust', function (e, useBody) { resetIframeSize(useBody || false); });
+	$(document).ajaxComplete(function () { resetIframeSize(false); });
+	$(document).ajaxStart(function () { resetIframeSize(false); });
 
 	var lastWindowSize = $(window).width();
 	var timeout = 0;
-	$(window).resize(function() {
+	$(window).resize(function () {
 		clearTimeout(timeout);
-		timeout = setTimeout(function() {
-			if(lastWindowSize != $(window).width()){
+		timeout = setTimeout(function () {
+			if (lastWindowSize != $(window).width()) {
 				resetIframeSize(true);
 				lastWindowSize = $(window).width();
 			}
 		}, 20);
 	});
-})();
+})(jQuery);
 
-$(function() {
-	$("a[href^=#]").click(function() {
-		window.parent.postMessage("qd-iframe-scroll|" + ($($(this).attr("href")).first().offset() || {top: 0}).top, "*");
+(function ($) {
+	$(function () {
+		$("a[href^=#]").click(function () {
+			window.parent.postMessage("qd-iframe-scroll|" + ($($(this).attr("href")).first().offset() || { top: 0 }).top, "*");
+		});
 	});
-});
+})(jQuery);
