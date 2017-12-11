864d12ae3081ea4f260a19448da2ee3c9312d691
Yuri Sparinger <yuri@quatrodigital.com.br>
2017-12-11 10:30:46 -0200

    ...

--- arquivos/qd-iframe-adjust.js
+++ arquivos/qd-iframe-adjust.js
@@ -11,7 +11,7 @@
 	};
 
 	$(function () { resetIframeSize(false); });
-	$(window).load(function () { resetIframeSize(false); });
+	$(window).on("load", function () { resetIframeSize(false); });
 	$(window).scroll(function () { resetIframeSize(false); });
 	$(window).on('QD_manualIframeAdjust', function (e, useBody) { resetIframeSize(useBody || false); });
 	$(document).ajaxComplete(function () { resetIframeSize(false); });
